1abbe9f4ac7430a27487c9d7aa42467cf4802dda
the-geka <theGeka@mail.ru>
2016-10-11 12:03:05 +0300

    clean import

--- src/test/java/part1/exercise/CompletableFutureBasics.java
+++ src/test/java/part1/exercise/CompletableFutureBasics.java
@@ -9,7 +9,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.security.cert.PKIXRevocationChecker;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
